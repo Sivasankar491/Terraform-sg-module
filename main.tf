29a6208369aacf970a3595bcfd34aafab6966eef
Sankar491 <sivasankarrcks@gmail.com>
2024-10-03 13:12:48 +0530

    changed the vpc in SG

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_security_group" "main" {
   name        = local.final_name
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = data.aws_vpc.default.id
-  # vpc_id      = var.vpc_id
+  # vpc_id      = data.aws_vpc.default.id
+  vpc_id      = var.vpc_id
 
   egress {
     from_port        = 0
